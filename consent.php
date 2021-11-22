ee6ec786e2bb4500194815a4f7c56a1d5520bd90
m226318 <92345731+m226318@users.noreply.github.com>
2021-11-22 02:46:44 -0500

    Update consent.php

--- consent.php
+++ consent.php
@@ -2,10 +2,11 @@
    if(isset($_POST['add'])) {
       $dbhost = 'localhost';
       $dbuser = 'root';
-      $dbpass = 'root@123';
-      $dbname = 'AI';
+      $dbpass = 'CAPst0ne12!@';
+      $dbname = 'aitrust';
       $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-
+      date_default_timezone_set('America/New_York');
+      $date = date('m/d/y h:i:s a',time());
       if($mysqli->connect_errno ) {
          printf("Connect failed: %s<br />", $mysqli->connect_error);
          exit();
@@ -15,7 +16,7 @@
 
       $sql = "INSERT INTO consent ".
          "(name, date) "."VALUES ".
-         "('$author')";
+         "('$author','$date')";
 
       if ($mysqli->query($sql)) {
          printf("Record inserted successfully.<br />");
