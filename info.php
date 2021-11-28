1aae5ddb2b6da7a8bf46724366f809b9f3903b22
m226318 <92345731+m226318@users.noreply.github.com>
2021-11-28 00:17:46 -0500

    Update info.php

--- info.php
+++ info.php
@@ -1,9 +1,19 @@
 hello
 <?php
+ $dbhost = 'localhost';
+      $dbuser = 'root';
+      $dbpass = 'CAPst0ne12!@';
+      $dbname = 'aitrust';
+      $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
+      if($mysqli->connect_errno ) {
+         printf("Connect failed: %s<br />", $mysqli->connect_error);
+         exit();
+      }
+      printf('Connected successfully.<br />');
       printf('Connected successfully.<br />');
       $username = $_POST['username'];
-$age = $_POST['age'];
+      $age = $_POST['age'];
       $field = $_POST['field'];
       $gpa = $_POST['gpa'];
       $trust = $_POST['trust'];
@@ -14,11 +24,11 @@ $age = $_POST['age'];
       $sql = "INSERT INTO info ".
          "(id,username,age,field,gpa,trust,feeling,help,gender,personalreccs) "."VALUES ".
          "(1,'$username','$age','$field','$gpa','$trust','$feeling',$help,'$gender','$personal')";
-printf($sql);
+
          printf("\n o);
       if ($mysqli->query($sql)) {
          printf("Record inserted successfully.<br />");
-      }printf("l");
+      }
       if ($mysqli->errno) {
          printf("Could not insert record into table: %s<br />", $mysqli->error);
       }
