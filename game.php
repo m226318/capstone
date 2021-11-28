465ec0d8f28b795d8578f432cebb54ec3c56820b
m226318 <92345731+m226318@users.noreply.github.com>
2021-11-28 08:21:19 -0500

    Create game.php

---
+++ game.php
@@ -0,0 +1,38 @@
+<?php
+ $dbhost = 'localhost';
+      $dbuser = 'root';
+      $dbpass = 'CAPst0ne12!@';
+      $dbname = 'aitrust';
+      $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+      if($mysqli->connect_errno ) {
+         printf("Connect failed: %s<br />", $mysqli->connect_error);
+         exit();
+      }
+      printf('Connected successfully.<br />');
+     
+      $username = $_POST['username'];
+      $age = $_POST['age'];
+      $field = $_POST['field'];
+      $gpa = $_POST['gpa'];
+      $trust = $_POST['trust'];
+      $feeling = $_POST['feeling'];
+      $help = $_POST['help'];
+      $gender=$_POST['gender'];
+      $personal=$_POST['personal'];
+   $sql = "INSERT INTO info ". //need to add cookies to store user id and replace 1 here
+         "(id,username,age,field,gpa,trust,feeling,help,gender,personalreccs) "."VALUES ".
+         "(1,'$username','$age','$field','$gpa','$trust','$feeling',$help,'$gender','$personal')";
+
+    
+printf($sql);
+if ($mysqli->query($sql)) {
+         printf("Record inserted successfully.<br />");
+      }
+      if ($mysqli->errno) {
+         printf("Could not insert record into table: %s<br />", $mysqli->error);
+      }
+$newURL="ins.html";
+$mysqli->close();
+header('Location: '.$newURL);
+?>
