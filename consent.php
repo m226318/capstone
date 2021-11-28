cbc8093bc4cdb7799d20206f43e6b165b17975c8
m226318 <92345731+m226318@users.noreply.github.com>
2021-11-28 00:09:21 -0500

    Update consent.php

--- consent.php
+++ consent.php
@@ -24,6 +24,26 @@
       if ($mysqli->errno) {
          printf("Could not insert record into table: %s<br />", $mysqli->error);
       }
+ $username = $_POST['username'];
+$age = $_POST['age'];
+      $field = $_POST['field'];
+      $gpa = $_POST['gpa'];
+      $trust = $_POST['trust'];
+      $feeling = $_POST['feeling'];
+      $help = $_POST['help'];
+      $gender=$_POST['gender'];
+      $personal=$_POST['personal'];
+      $sql = "INSERT INTO info ".
+         "(id,username,age,field,gpa,trust,feeling,help,gender,personalreccs) "."VALUES ".
+         "(1,'$username','$age','$field','$gpa','$trust','$feeling',$help,'$gender','$personal')";
+printf($sql);
+         printf("\n o);
+      if ($mysqli->query($sql)) {
+         printf("Record inserted successfully.<br />");
+      }printf("l");
+      if ($mysqli->errno) {
+         printf("Could not insert record into table: %s<br />", $mysqli->error);
+      }
       $mysqli->close();
    
 $newURL="information.html";
