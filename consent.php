dd6f10cfc9645213c0d269ce3263b9394b29a24b
m226318 <92345731+m226318@users.noreply.github.com>
2021-12-01 01:48:23 -0500

    Update consent.php

--- consent.php
+++ consent.php
@@ -24,12 +24,18 @@
       if ($mysqli->errno) {
          printf("Could not insert record into table: %s<br />", $mysqli->error);
       }
-printf("ln");
+
       $sql = "SELECT id FROM consent where name='$author'and date = '$date' ";
-printf("lnn");
+
       $result = $mysqli->query($sql);
-printf("lnnn");
-      printf($result);
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
   printf("lnnnn");
       $sql = "SELECT id FROM consent where name=\'$author\'and date = \'$date\' ";
       $result = $mysqli->query($sql);
