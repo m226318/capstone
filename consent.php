4a3ed1e958e809afab95edd65737a11cfe05a9cc
m226318 <92345731+m226318@users.noreply.github.com>
2021-12-01 01:45:21 -0500

    Update consent.php

--- consent.php
+++ consent.php
@@ -31,6 +31,14 @@
   printf("n");
       $sql = "SELECT id FROM consent where name=\'$author\'and date = \'$date\' ";
       $result = $mysqli->query($sql);
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
       printf($result);
       printf("end");
    $mysqli->close();
